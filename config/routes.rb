5975476b708b766ad3f60e777c2901241ef66e3b
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-11-07 21:55:11 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/blogs', 'blogs#index'
+  get '/blogs' ,to:'blogs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
