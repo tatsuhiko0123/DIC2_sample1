3f2bd8917e7e30f95bdb5c7f8de6ea7919847376
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-11-16 22:42:33 +0900

    Strong Parametersの設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,15 @@
 class BlogsController < ApplicationController
   def index
   end
-
   def new
     @blog = Blog.new
   end
-
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    Blog.create(blog_params)
     redirect_to new_blog_path
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
