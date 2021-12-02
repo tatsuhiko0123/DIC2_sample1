d2f79c579bb60b0d4775c6309f74247f071fc70b
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-12-02 22:19:06 +0900

    コントローラにupdateメソッド追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,6 +19,14 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.find(params[:id])
   end
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
